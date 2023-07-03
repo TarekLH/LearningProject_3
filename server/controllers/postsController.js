5a4d8d10b5045133fc9c4b585454a30f88c0394e
TarekLH <lahouaouitarek99@gmail.com>
2023-07-04 00:38:32 +0100

    adding pagination on main page

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -8,8 +8,28 @@ async function getPosts(req, res) {
       title: "NodeJs Blog",
       description: "Simple blog created with NodeJs, Express and mongoDB."
     };
-    const datas = await postModel.find({}).sort({createdAt: -1});
-    res.render('index', {locals, datas});
+
+    // paginations query
+    let perPage = 7;
+    let page = req.query.page || 1;
+
+    // take posts in descending order and skip the posts from the previous pages
+    const datas = await postModel.aggregate([ { $sort: { createdAt: -1 }}])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    const totalPosts = await postModel.count();
+    const nextPage = parseInt(page) + 1;
+    // determines whether there is a next page available by comparing (nextPage) with the maximum number of pages needed to display all the documents
+    const hasNextPage = nextPage <= Math.ceil(totalPosts / perPage);
+
+    res.render('index', {
+      locals,
+      datas,
+      currPage: page,
+      nextPage: hasNextPage ? nextPage : null
+    });
   } catch (error) {
     console.log(error);
   }
